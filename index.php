bcc51ee3567c8b21d510dd90855ad9a7eb84fbba
Wojtek <wchybiorz@gmail.com>
2015-06-27 10:59:28 +0200

    pierwszy commit

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+require_once '/lib/Twig/Autoloader.php';
+Twig_Autoloader::register();
+
+$loader = new Twig_Loader_Filesystem('/templates');
+$twig = new Twig_Environment($loader, array(
+    'cache' => '/cache',
+));
+?>
